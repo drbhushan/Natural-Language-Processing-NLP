97f9bc87663eaac5a76caf45877c0c61fb012cf1
S. N. Bharath Bhushan <sn.bharath.code@gmail.com>
2020-05-10 19:13:45 +0530

    Add files via upload

---
+++ NLP-1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 23:36:28 2019
+
+@author: Bharath
+"""
+
+word = "Bonjour World!"
+print(word)
+print('Bonjour World!')
+print('word')
+print(word[0])
+print(len(word))
+print(word.count("o"))
+print(word.find("j"))
+print(word.index("World"))
+print(word.upper())
+print(word.lower())
+print(word.title())
+print(word.capitalize())
+print(word.swapcase())
+print(print(word + "Fromage!"))
+print(print("hello " * 5))
+#print(print ''.join(reversed(word)))
+#print(print " ".join(word))
+print(word.replace("World", "Pizza"))
+print(word[8:16])
+print(word[0:7])
---
+++ NLP-2.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 23:40:10 2019
+
+@author: Bharath
+"""
+
+
+#virtualenv env
+#source env/bin/activate
+#pip install spacy
+
+# out-of-the-box: download best-matching default model
+#spacy download en # english model
+#spacy download de # german model
+#spacy download es # spanish model
+#spacy download fr # french model
+#spacy download xx # multi-language model
+# download best-matching version of specific model for your spaCy installation
+#spacy download en_core_web_sm
+# download exact model version (doesn't create shortcut link)
+#spacy download en_core_web_sm-2.0.0 --direct
+
+# using pip to install models
+#pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_md-1.2.0/en_core_web_md-1.2.0.tar.gz
+# with local file
+#pip install /Users/you/en_core_web_md-1.2.0.tar.gz
+
+
+# Using spaCy
+
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+doc = nlp(u'This is a sentence.')
+
+# Using particular model
+
+#import en_core_web_md
+#nlp = en_core_web_md.load()
+#doc = nlp(u'This is a sentence.')
+
+# Constructing tokenizer
+
+#nlp = spacy.load('en')
+#nlp.tokenizer = my_tokenizer
+
+# printing POS
+
+doc = nlp(u'Narendra Damodardas Modi is an Indian politician serving as the 14th and current Prime Minister of India since 2014')
+for token in doc:
+    print(token.text, token.pos_)
+
+# printing NER
+
+doc = nlp(u'Microsoft has offices all over Europe.')
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+# Adding stop words
+my_stop_words = [u'say', u'be', u'said', u'says', u'saying']
+for stopword in my_stop_words:
+    lexeme = nlp.vocab[stopword]
+    lexeme.is_stop = True
+
+# pre-processing text - lematization
+doc = nlp(u'I have studied in various universities')
+sentence = []
+for w in doc:
+    if w.text != '\n' and not w.is_stop and not w.is_punct and not w.like_num:
+        sentence.append(w.lemma_)
+print(sentence)
